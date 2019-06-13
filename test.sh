81b15120f82cccdbc085963e36fe424fabc92cfc
piotrekwoznicki <piotrekwoznicki@gmail.com>
2019-06-13 23:44:10 +0200

    add requirements

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python nnunet/inference/predict_simple.py -i /home/piotr/kidney_seg/example/single_input -o /home/piotr/kidney_seg/example/pred -t Task00_DATASET -tr nnUNetTrainer -m 2d --tta 0 --num_threads_preprocessing 12 --num_threads_nifti_save 8
+python nnunet/inference/predict_simple.py -i /home/piotr/kidney_seg/example/single_input -o /home/piotr/kidney_seg/example/pred -t Task00_DATASET -tr nnUNetTrainer -m 2d --tta 0 --num_threads_preprocessing 1 --num_threads_nifti_save 1
